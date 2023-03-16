0125239e3820f2139331b048bfd4490d50ff41e2
sungminyang <lonesome661@naver.com>
2023-03-17 00:05:04 +0900

    redis repository and r2dbc repository init

--- src/main/java/com/scm/module/Repository/UserRedisRepository.java
+++ src/main/java/com/scm/module/Repository/UserRedisRepository.java
@@ -1,14 +1,12 @@
 package com.scm.module.Repository;
 
 import com.scm.module.Model.UserEntity;
-import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
-public interface UserRepository extends ReactiveCrudRepository<UserEntity, Long> {
-
-    @Query("SELECT * FROM users WHERE email_address = :email")
-    Mono<UserEntity> findByEmail(String email);
+public interface UserRedisRepository extends ReactiveCrudRepository<UserEntity, Long> {
+    Mono<UserEntity> findByEmail(@Param("email") String email);
 }
